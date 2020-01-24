683a6b6f9b172bfb7a70c31da37c6c9112e6a741
bharathi43 <42730804+bharathi43@users.noreply.github.com>
2020-01-24 17:21:44 +0530

    Add files via upload

---
+++ browser.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+pyautogui.position()
+pyautogui.click(381,766)
+time.sleep(2)
+pyautogui.typewrite("https://jm39507.us-east-2.aws.snowflakecomputing.com/console#/internal/worksheet")
+pyautogui.typewrite(["ENTER"])
